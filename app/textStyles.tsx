3a5a45e40142dddee90792bd18fd90c3d84d5225
Daniel Kantor <git@daniel-kantor.com>
2023-11-15 00:03:48 +0100

    add some more text styles

--- app/textStyles.tsx
+++ app/textStyles.tsx
@@ -2,15 +2,45 @@ import { defineTextStyles } from '@pandacss/dev'
 
 export const textStyles = defineTextStyles({
   body: {
-    description: 'The body text style - used in paragraphs',
+    description: 'Used in paragraphs',
     value: {
       fontFamily: 'Shantell Sans Variable, sans-serif',
       fontWeight: '500',
       fontSize: '18px',
-      lineHeight: '27px',
-      letterSpacing: '0',
-      textDecoration: 'None',
-      textTransform: 'None'
+      lineHeight: '28px',
     }
-  }
+  },
+  heading1: {
+    description: 'Used in <h1 /> tags',
+    value: {
+      fontFamily: 'Shantell Sans Variable, sans-serif',
+      fontWeight: '700',
+      fontSize: '90px',
+      lineHeight: '56px',
+      marginTop: '56px',
+      marginBottom: '56px',
+    }
+  },
+  heading2: {
+    description: 'Used in <h2 /> tags',
+    value: {
+      fontFamily: 'Shantell Sans Variable, sans-serif',
+      fontWeight: '700',
+      fontSize: '56px',
+      lineHeight: '56px',
+      marginTop: '28px',
+      marginBottom: '28px',
+    }
+  },
+  heading3: {
+    description: 'Used in <h3 /> tags',
+    value: {
+      fontFamily: 'Shantell Sans Variable, sans-serif',
+      fontWeight: '700',
+      fontSize: '36px',
+      lineHeight: '56px',
+      marginTop: '28px',
+      marginBottom: '28px',
+    }
+  },
 })
