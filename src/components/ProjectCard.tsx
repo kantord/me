474cadd6456ddbb6f0da6405c7dd804979105581
Daniel Kantor <git@daniel-kantor.com>
2026-06-22 20:18:56 +0200

    .

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,11 +7,17 @@ export interface ProjectCardData {
 
 function ProjectCard({ href, image, title, description }: ProjectCardData) {
   return (
-    <li>
-      <a href={href}>
-        <span className="img" style={{ backgroundImage: `url('${image}')` }} />
-        <span>{title}</span>
-        <span>{description}</span>
+    <li className="basis-full ml-[1em]">
+      <a
+        href={href}
+        className="text-[0.9rem] h-40 p-2 overflow-hidden flex bg-[#e9f0f3] flex-col no-underline text-inherit"
+      >
+        <span
+          className="self-center block w-36 h-16 bg-cover bg-center mb-4 bg-white"
+          style={{ backgroundImage: `url('${image}')` }}
+        />
+        <span className="flex text-[0.9rem] font-semibold">{title}</span>
+        <span className="flex text-[0.65rem]">{description}</span>
       </a>
     </li>
   )
@@ -19,8 +25,8 @@ function ProjectCard({ href, image, title, description }: ProjectCardData) {
 
 export function CardRow({ projects }: { projects: ProjectCardData[] }) {
   return (
-    <div className="cards">
-      <ul>
+    <div className="-mt-4 ml-2">
+      <ul className="flex">
         {projects.map((project) => (
           <ProjectCard key={project.title} {...project} />
         ))}
