84d16aebc7c8453bf5fe4f9f52bc81219cf2d34b
Daniel Kantor <git@daniel-kantor.com>
2026-06-22 20:00:42 +0200

    .

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -3,9 +3,9 @@ export function ContactInfo() {
     <div className="absolute top-[3.618rem] right-[5.618rem] max-[700px]:static">
       <p className="!m-0 max-[700px]:!my-8">
         www.daniel-kantor.com/cv <br />
-        <em>Email</em>: cv@daniel-kantor.com <br />
-        <em>Location</em>: Barcelona, Spain <br />
-        <em>Date of Birth</em>: May 1993 <br />
+        <em className="font-semibold not-italic">Email</em>: cv@daniel-kantor.com <br />
+        <em className="font-semibold not-italic">Location</em>: Barcelona, Spain <br />
+        <em className="font-semibold not-italic">Date of Birth</em>: May 1993 <br />
       </p>
     </div>
   )
--- src/components/SkillList.tsx
+++ src/components/SkillList.tsx
@@ -9,7 +9,7 @@ const itemClass = 'inline-block mx-[0.2rem] mb-2 leading-[1.2] py-[0.15rem] px-[
 
 function SkillItem({ name, level }: Skill) {
   if (level === 'strong') return <li className={itemClass}><strong>{name}</strong></li>
-  if (level === 'em') return <li className={itemClass}><em className="text-[0.9rem]">{name}</em></li>
+  if (level === 'em') return <li className={itemClass}><em className="text-[0.9rem] font-semibold not-italic">{name}</em></li>
   if (level === 'span') return <li className={itemClass}><span className="text-[0.8rem]">{name}</span></li>
   return <li className={itemClass}>{name}</li>
 }
